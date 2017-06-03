d8a1f1c06b817783caae88a22306e6c8d79649fa
Felix Geyer <debfx@fobos.de>
2017-06-03 12:06:48 +0200

    Remove unused import.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="runjail",
